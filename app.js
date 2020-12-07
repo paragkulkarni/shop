0e8c5cf481ef6783517a3f564493111567365ee7
parag <paragkk80@gmail.com>
2020-12-07 18:33:04 +0530

    stylesheet added

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const adminRoutes = require('./routes/admin');
-
+const shopRoutes = require('./routes/shop');
+const rootDir = require('./util/path');
+const path = require('path')
 const app = express();
 
 const bodyParser = require('body-parser');
 
 
 app.use(bodyParser.urlencoded({urlencoded: false}));
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
+
+app.use(shopRoutes);
 
 
 app.use((req, res, next) => {
-    res.status(404).send("<h1>Page not found</h1>")
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000, () => {
---
+++ routes/shop.js
@@ -0,0 +1,11 @@
+const rootDir = require('../util/path');
+const path = require('path');
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next)=>{
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+});
+
+
+module.exports = router;

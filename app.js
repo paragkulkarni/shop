40a589fcc5677ae3457e92c22a7eb3b559f3caf3
parag <paragkk80@gmail.com>
2020-12-11 21:06:06 +0530

    controller is added

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const rootDir = require('./util/path');
-const path = require('path')
+const path = require('path');
+const errorsData = require('./controllers/errors');
+
+
 const app = express();
 
 const bodyParser = require('body-parser');
@@ -23,10 +25,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 
-app.use((req, res, next) => {
-    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-    res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
+app.use(errorsData.get404);
 
 app.listen(3000, () => {
     console.log('App listening on port 3000!');
--- routes/shop.js
+++ routes/shop.js
@@ -1,20 +1,9 @@
-const rootDir = require('../util/path');
-const path = require('path');
 const express = require('express');
 const router = express.Router();
-const adminData = require('./admin');
+const productData = require('../controllers/products');
 
-router.get('/', (req, res, next)=>{
-    const products = adminData.products;
-    // res.sendFile(path.join(rootDir, 'views', 'shop.html'), admin.products);
-    res.render('shop', {prods: products,
-    pageTitle: 'Shop',
-    path: '/',
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true
-    });
-});
+
+router.get('/', productData.getProducts);
 
 
 module.exports = router;
